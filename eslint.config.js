a7f279a19f72e7e6b53588725e04c01364d3196d
Jeongin Lee <jil8885@gmail.com>
2025-02-14 21:44:38 +0900

    CI/CD: Run code style check for every commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,32 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react': eslintPluginReact,
+            'react-hooks': eslintPluginReactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            'semi': ['error', 'never'],
+            'indent': ['error', 4],
+            ...eslintPluginReact.configs.flat.recommended.rules,
+            ...eslintPluginReactHooks.configs.recommended.rules,
+            'react/jsx-uses-vars': 'error',
+            'react/react-in-jsx-scope': 'off',
+            'react-hooks/rules-of-hooks': 'error'
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
 )
