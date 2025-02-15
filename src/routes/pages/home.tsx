047cf36127c98ed8a5b984588d03ab01e1984097
Jeongin Lee <jil8885@gmail.com>
2025-02-15 22:55:33 +0900

    feat: Show nickname from API response

--- src/routes/pages/home.tsx
+++ src/routes/pages/home.tsx
@@ -1,41 +1,30 @@
 import { getUserInfo } from "../../service/network/auth.ts"
-import { useAuthenticatedStore } from "../../stores/auth.ts"
+import { useAuthenticatedStore, useUserInfoStore } from "../../stores/auth.ts"
 import { AxiosError } from "axios";
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu'
 import LogOutIcon from '@mui/icons-material/Logout'
+import { useEffect } from "react";
 
-const logOutButtonClicked = () => {
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('refreshToken')
-    window.location.href = '/login'
-}
-
-// App bar
-const appBar = (
-    <AppBar position="static">
-        <Toolbar>
-            <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                휴아봇 서비스 관리자 페이지
-            </Typography>
-            <IconButton size="large" color="inherit" onClick={logOutButtonClicked}>
-                <LogOutIcon />
-            </IconButton>
-        </Toolbar>
-    </AppBar>
-)
 
 export default function Home() {
     // Get the store
     const isAuthenticatedStore = useAuthenticatedStore()
+    const userInfoStore = useUserInfoStore()
     // Fetch user info
     const fetchUserInfo = async () => {
         try {
             const response = await getUserInfo()
             isAuthenticatedStore.setIsAuthenticated(response.status === 200)
+            if (response.status === 200) {
+                const responseData = response.data
+                userInfoStore.setUserInfo(
+                    responseData.username,
+                    responseData.nickname,
+                    responseData.email,
+                    responseData.phone,
+                )
+            }
         } catch (error) {
             const err = error as AxiosError
             if (err.response?.status === 401) {
@@ -43,9 +32,37 @@ export default function Home() {
             }
         }
     }
+    // Log out button clicked
+    const logOutButtonClicked = () => {
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+        window.location.href = '/login'
+    }
     // Fetch user info when the component is mounted
-    fetchUserInfo().then()
+    useEffect(() => {
+        fetchUserInfo().then()
+    }, [])
 
+    // Component
+    // App bar
+    const appBar = (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    휴아봇 서비스 관리자 페이지
+                </Typography>
+                <Typography variant="h6" component="div" style={{ textAlign: "end" }} sx={{ flexGrow: 1 }}>
+                    {userInfoStore.nickname}
+                </Typography>
+                <IconButton size="large" color="inherit" onClick={logOutButtonClicked}>
+                    <LogOutIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    )
     if (isAuthenticatedStore.isAuthenticated === null) {
         return <h1>Loading</h1>
     } else if (isAuthenticatedStore.isAuthenticated) {
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -5,7 +5,23 @@ type AuthenticatedStore = {
     setIsAuthenticated: (isAuthenticated: boolean) => void,
 }
 
+type UserInfoStore = {
+    username: string | null,
+    nickname: string | null,
+    email: string | null,
+    phone: string | null,
+    setUserInfo: (username: string, nickname: string, email: string, phone: string) => void,
+}
+
 export const useAuthenticatedStore = create<AuthenticatedStore>((set) => ({
     isAuthenticated: null,
     setIsAuthenticated: (isAuthenticated: boolean) => set({ isAuthenticated }),
+}))
+
+export const useUserInfoStore = create<UserInfoStore>((set) => ({
+    username: null,
+    nickname: null,
+    email: null,
+    phone: null,
+    setUserInfo: (username: string, nickname: string, email: string, phone: string) => set({ username, nickname, email, phone }),
 }))
