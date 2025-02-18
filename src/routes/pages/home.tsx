ab74aa9691a4ab2eb2c2a4b84a9f9280bf5cf3e3
Jeongin Lee <jil8885@gmail.com>
2025-02-18 22:42:15 +0900

    feat: Implement hamburger menu and drawer

--- src/routes/pages/home.tsx
+++ src/routes/pages/home.tsx
@@ -1,15 +1,36 @@
+import { createElement, useEffect } from "react"
+import {
+    AppBar,
+    Box,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon, ListItemText,
+    Toolbar,
+    Typography
+} from "@mui/material"
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import ContactsIcon from '@mui/icons-material/Contacts'
+import DepartureBoardIcon from '@mui/icons-material/DepartureBoard'
+import DiningIcon from '@mui/icons-material/Dining'
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus'
+import DirectionsSubwayIcon from '@mui/icons-material/DirectionsSubway'
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks'
+import LogOutIcon from '@mui/icons-material/Logout'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useDrawerOpenedStore } from "../../stores/home.ts"
 import { getUserInfo } from "../../service/network/auth.ts"
 import { useAuthenticatedStore, useUserInfoStore } from "../../stores/auth.ts"
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
-import MenuIcon from '@mui/icons-material/Menu'
-import LogOutIcon from '@mui/icons-material/Logout'
-import { useEffect } from "react";
 
 
 export default function Home() {
     // Get the store
     const isAuthenticatedStore = useAuthenticatedStore()
     const userInfoStore = useUserInfoStore()
+    const drawerOpenedStore = useDrawerOpenedStore()
     // Fetch user info
     const fetchUserInfo = async () => {
         const response = await getUserInfo()
@@ -30,6 +51,10 @@ export default function Home() {
         localStorage.removeItem('refreshToken')
         window.location.href = '/login'
     }
+    // Menu button clicked
+    const menuButtonClicked = () => {
+        drawerOpenedStore.setDrawerOpened(true)
+    }
     // Fetch user info when the component is mounted
     useEffect(() => {
         fetchUserInfo().then()
@@ -38,9 +63,15 @@ export default function Home() {
     // Component
     // App bar
     const appBar = (
-        <AppBar position="static">
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Toolbar>
-                <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                    onClick={() => drawerOpenedStore.setDrawerOpened(!drawerOpenedStore.isDrawerOpened)}>
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -55,12 +86,55 @@ export default function Home() {
             </Toolbar>
         </AppBar>
     )
+    // Drawer
+    const menuTexts = ['셔틀버스', '노선버스', '전철', '학식', '열람실', '연락처', '학사일정', '사용자']
+    const menuIcons = [
+        DepartureBoardIcon,
+        DirectionsBusIcon,
+        DirectionsSubwayIcon,
+        DiningIcon,
+        LibraryBooksIcon,
+        ContactsIcon,
+        CalendarMonthIcon,
+        AccountBoxIcon,
+    ]
+    const menuItemClicked = (text: string) => {
+        console.log(text)
+    }
     if (isAuthenticatedStore.isAuthenticated === null) {
         return <h1>Loading</h1>
     } else if (isAuthenticatedStore.isAuthenticated) {
         return (
             <div>
                 {appBar}
+                <Drawer
+                    open={drawerOpenedStore.isDrawerOpened}
+                    onClose={menuButtonClicked}
+                    sx={{
+                        width: 240,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: 240,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    anchor="left">
+                    <Toolbar />
+                    <Box sx={{ overflow: 'auto' }}>
+                        <List>
+                            {menuTexts.map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton onClick={() => menuItemClicked(text)}>
+                                        <ListItemIcon>
+                                            {createElement(menuIcons[index])}
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                </Drawer>
                 <h1>Home</h1>
             </div>
         )
