e6d1813decd151fe918f264af27fd09923f4fb8b
Jeongin Lee <jil8885@gmail.com>
2025-02-26 22:17:41 +0900

    refactor: Implement refresh button on toolbar

--- src/routes/pages/shuttle/period/toolbar.tsx
+++ src/routes/pages/shuttle/period/toolbar.tsx
@@ -1,12 +1,31 @@
 import { v4 as uuidv4 } from "uuid"
 import { Button } from "@mui/material"
 import { GridRowModes, GridToolbarContainer } from "@mui/x-data-grid"
-import AddIcon from "@mui/icons-material/Add"
 import { useShuttlePeriodGridModelStore, useShuttlePeriodStore } from "../../../../stores/shuttle.ts"
+import { getShuttlePeriod, ShuttlePeriodResponse } from "../../../../service/network/shuttle.ts"
+
+import AddIcon from "@mui/icons-material/Add"
+import RefreshIcon from '@mui/icons-material/Refresh'
 
 export function Toolbar() {
     const rowStore = useShuttlePeriodStore()
     const rowModesModelStore = useShuttlePeriodGridModelStore()
+    // Fetch shuttle period
+    const fetchShuttlePeriod = async () => {
+        const response = await getShuttlePeriod()
+        if (response.status === 200) {
+            const responseData = response.data
+            rowStore.setRows(responseData.data.map((period: ShuttlePeriodResponse) => {
+                return {
+                    id: uuidv4(),
+                    type: period.type,
+                    start: period.start,
+                    end: period.end,
+                }
+            }))
+        }
+    }
+    // Add record button click event
     const addRowButtonClicked = () => {
         const id = uuidv4()
         rowStore.setRows([
@@ -26,9 +45,12 @@ export function Toolbar() {
     }
 
     return (
-        <GridToolbarContainer>
-            <Button color="primary" startIcon={<AddIcon />} onClick={addRowButtonClicked}>
-                Add record
+        <GridToolbarContainer style={{ display: "flex", justifyContent: "flex-end", marginTop: "10px" }}>
+            <Button color="primary" variant="outlined" startIcon={<RefreshIcon />} onClick={fetchShuttlePeriod}>
+                새로고침
+            </Button>
+            <Button color="primary" variant="contained" startIcon={<AddIcon />} onClick={addRowButtonClicked}>
+                항목 추가
             </Button>
         </GridToolbarContainer>
     )
