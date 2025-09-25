16d247cba17753b6437e611d9b71b17b86489221
Jeongin Lee <jil8885@gmail.com>
2025-09-25 16:15:33 +0900

    refactor: Height of shuttle grid

--- src/routes/pages/shuttle/route/grid.tsx
+++ src/routes/pages/shuttle/route/grid.tsx
@@ -138,7 +138,7 @@ export const ShuttleRouteGrid = (props: GridProps) => {
     })
     // Render
     return (
-        <Box sx={{ height: '100vh', width: '100%' }}>
+        <Box sx={{ height: '90vh', width: '100%' }}>
             <Snackbar
                 anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
                 open={errorSnackbarContent !== ''}
